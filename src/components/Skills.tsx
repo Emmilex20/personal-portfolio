2632a0f0c0bf9bdfd7aa62964d39c80eecf61078
Emmanuel Raphael <aginaemmanuel6@gmail.com>
2024-12-23 12:27:00 +0100

    Resolved skills responsiveness

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,11 +13,11 @@ const Skills = () => {
     <div className="bg-[linear-gradient(to_top,#000,#381a5f_80%)] py-32">
       <div className="text-white w-[400px] md:min-w-[950px] mx-auto p-8 text-center">
         <h2 className="text-6xl font-bold mb-4">What I Do</h2>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
           {skillIcons.map((skill, index) => (
             <div
               key={index}
-              className="h-[160px] w-[160px] md:h-[220px] md:w-[220px] flex flex-col justify-between items-center bg-white/10 p-4 rounded-xl transition-transform transform hover:scale-105 hover:shadow-lg"
+              className="h-[160px] w-[160px] sm:h-[200px] sm:w-[200px] md:h-[220px] md:w-[220px] flex flex-col justify-between items-center bg-white/10 p-4 rounded-xl transition-transform transform hover:scale-105 hover:shadow-lg"
             >
               {skill.icon}
               <p className="mt-2 text-lg font-medium">{skill.label}</p>
