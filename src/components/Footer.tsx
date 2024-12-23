f2c8a8dc65472ab9b563d4b925991e7e31cc495f
Emmanuel Raphael <aginaemmanuel6@gmail.com>
2024-12-23 12:22:18 +0100

    Resolved responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import { FaLinkedin, FaTwitter, FaFacebook } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div className="mt-12 text-white/70 py-8 max-w-[1000px] mx-auto border-t border-gray-700 pt-4 flex justify-between items-center">
+        <div className="mt-12 text-white/70 py-8 max-w-[1000px] mx-auto border-t border-gray-700 pt-4 flex flex-col sm:flex-row justify-between items-center">
            
-            <h1 className="text-2xl font-bold">Emmanuel Agina</h1>
+            <h1 className="text-2xl font-bold text-center sm:text-left">Emmanuel Agina</h1>
 
-            <div className='flex space-x-6 mt-4'>
+            <div className='flex space-x-6 mt-4 sm:mt-0'>
                 <a href="https://www.linkedin.com/in/chidozie-agina-2a50742a8/" target="_blank" rel="noopener noreferrer" className='hover:text-gray-300'>
                     <FaLinkedin size={24}/>
                 </a>
